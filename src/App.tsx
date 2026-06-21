3d2fbc1cebbf73fc9ad255c167a069aca58123e0
Boris Nedeltchev <borisnedeltchev@Boriss-MacBook-Air.local>
2026-06-21 19:56:41 +0200

    fix home wp fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
@@ -11,6 +12,7 @@ import Newsletter from "./pages/Newsletter";
 export default function App() {
   return (
     <Layout>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/journal" element={<Journal />} />
